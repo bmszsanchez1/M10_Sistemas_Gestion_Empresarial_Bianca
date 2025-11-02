b4a4d2975bd3e46202fb7bb95a094c45a2d40798
bmszsanchez1 <bmszsanchez@alumnes.ilerna.com>
2025-11-02 14:30:23 +0100

    Añadido el modulo Ejemplo01-HolaMundo

---
+++ Ejemplo01-HolaMundo/__manifest__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Ejemplo01 - Hola Mundo',
+    'version': '1.0',
+    'summary': 'Primer módulo de prueba',
+    'description': 'Módulo básico de ejemplo para Odoo',
+    'author': 'Bianca Sánchez',
+    'category': 'Tutorial',
+    'installable': True,
+    'application': True,
+}
